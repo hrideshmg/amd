0a56322391873c676efb99923d6e4125f36bda3c
chandramouli <mouli7666@gmail.com>
2025-02-21 20:05:50 +0530

    added an exception for mouli as well

--- src/tasks/status_update.rs
+++ src/tasks/status_update.rs
@@ -167,8 +167,8 @@ async fn collect_updates(channel_ids: &[ChannelId], ctx: &Context) -> anyhow::Re
                     (content.contains("namah shivaya")
                         && content.contains("regards")
                         && msg.timestamp >= yesterday_five_pm.into())
-                        || (content.contains("regards")
-                            && msg.author.name == "amanoslean"
+                        || ((content.contains("regards")
+                            && (msg.author.name == "amanoslean" || msg.author.name == "mouli7666")
                             && msg.timestamp >= yesterday_five_pm.into())
                 }));
             }
@@ -201,8 +201,8 @@ async fn collect_updates(channel_ids: &[ChannelId], ctx: &Context) -> anyhow::Re
                     (content.contains("namah shivaya")
                         && content.contains("regards")
                         && msg.timestamp >= yesterday_five_pm.into())
-                        || (content.contains("regards")
-                            && msg.author.name == "amanoslean"
+                        || ((content.contains("regards")
+                            && (msg.author.name == "amanoslean" || msg.author.name == "mouli7666"))
                             && msg.timestamp >= yesterday_five_pm.into())
                 }));
             }
